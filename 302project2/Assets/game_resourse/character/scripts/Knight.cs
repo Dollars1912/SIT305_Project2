1eec59266e2d9eda4a953159d65af66718021259
Lawrence Chu <DOLLARS1912@192-168-1-4.tpgi.com.au>
2018-04-21 21:27:48 +1000

    create character/ create transform door

---
+++ 302project2/Assets/game_resourse/character/scripts/Knight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour {
+
+    public Rigidbody2D rigidBody;
+	private Vector2 move = Vector2.zero;
+
+	public float verticalTopSpeed = 2f;
+	public float horizontalTopSpeed = 8f;
+    public float gravity = 20f;
+    public float jumpSpeed = 5f; 
+
+	// Use this for initialization
+	void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		move = new Vector2(Input.GetAxis("Horizontal"), 0);
+		if (Input.GetButton("Jump"))
+			move.y = jumpSpeed;
+
+        move = new Vector2(move.x * horizontalTopSpeed, move.y * verticalTopSpeed);
+        move.y -= gravity * Time.deltaTime;
+        var currentVelocity = rigidBody.velocity;
+
+        rigidBody.velocity = new Vector2(move.x, move.y);
+   	}   
+}
