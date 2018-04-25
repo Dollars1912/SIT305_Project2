04314a2c10240d65a45cade0dc21f6d8dd35fadb
inobtsp <576515628@qq.com>
2018-04-26 04:35:30 +1000

    add animation

--- 302project2/Assets/game_resourse/character/scripts/Knight.cs
+++ 302project2/Assets/game_resourse/character/scripts/Knight.cs
@@ -4,29 +4,64 @@ using UnityEngine;
 
 public class Knight : MonoBehaviour {
 
-    public Rigidbody2D rigidBody;
-	private Vector2 move = Vector2.zero;
-
-	public float verticalTopSpeed = 2f;
-	public float horizontalTopSpeed = 8f;
-    public float gravity = 20f;
-    public float jumpSpeed = 5f; 
-
-	// Use this for initialization
-	void Start () {
-	    	
+    
+    [Tooltip("this is a the grradient for horoizontal speed")]
+    public float speedBoost = 5f;
+    public float jumpspeed;
+    bool isjump;
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator anim;
+    
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		move = new Vector2(Input.GetAxis("Horizontal"), 0);
-		if (Input.GetButton("Jump"))
-			move.y = jumpSpeed;
 
-        move = new Vector2(move.x * horizontalTopSpeed, move.y * verticalTopSpeed);
-        move.y -= gravity * Time.deltaTime;
-        var currentVelocity = rigidBody.velocity;
-
-        rigidBody.velocity = new Vector2(move.x, move.y);
+        float playerSpeed = Input.GetAxisRaw("Horizontalmove");
+       playerSpeed *= speedBoost;
+        if (playerSpeed != 0)
+            Horizontalmoves(playerSpeed);
+        else
+            stopmoving();
+        
+        if (Input.GetButtonDown("Jump"))
+            jump();
    	}   
+
+
+    void Horizontalmoves(float playerSpeed)
+    {
+        rb.velocity = new Vector2(playerSpeed, rb.velocity.y);
+        print(rb.velocity);
+        if (playerSpeed < 0)
+            sr.flipX = true;
+        else if (playerSpeed > 0)
+            sr.flipX = false;
+        if(!isjump)
+            anim.SetInteger("state", 1);
+    }
+    void stopmoving()
+    {
+        rb.velocity = new Vector2(0, rb.velocity.y);
+        if(!isjump)
+            anim.SetInteger("state", 0);
+    }
+    void jump()
+    {
+        rb.AddForce(new Vector2(0, jumpspeed));
+        anim.SetInteger("state", 2);
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            isjump = false;
+        }
+    }
 }
