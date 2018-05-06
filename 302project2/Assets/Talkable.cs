3341a5f1399913f0808ab7388ea5b795ae1aeeaf
Lawrence <lawrencechu26@gmail.com>
2018-05-07 00:02:34 +1000

    add store dialog and NPCdialog

---
+++ 302project2/Assets/Talkable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class Talkable : MonoBehaviour {
+    public Flowchart talkFlowchart;
+    public string onCollosionEnter;
+
+    private void OnCollisionEnter(UnityEngine.Collider2D other)
+    {
+        if (other.gameObject.CompareTag("player")){
+            Block targetBlock = talkFlowchart.FindBlock(onCollosionEnter);
+            talkFlowchart.ExecuteBlock(targetBlock);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
