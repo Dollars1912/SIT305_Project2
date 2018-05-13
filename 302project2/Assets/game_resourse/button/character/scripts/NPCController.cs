4d1113e87cfac909e21f0ae558e37d80298b40f7
Lawrence <lawrencechu26@gmail.com>
2018-05-13 14:54:54 +1000

    add controller for NPC dialog

---
+++ 302project2/Assets/game_resourse/button/character/scripts/NPCController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using Fungus;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour {
+
+    public string BlockName;
+    private Flowchart flowChart;
+    private GameObject knight;
+
+    private void Awake()
+    {
+        flowChart = FindObjectOfType<Flowchart>();
+		knight = FindObjectOfType<Knight>().gameObject;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other == knight.GetComponent<Collider2D>())
+		{
+			StartCoroutine(Coroutine());
+		}
+	}
+
+	private IEnumerator Coroutine()
+	{
+		while (!Input.GetButton("Fire1"))
+			yield return null;
+
+        flowChart.ExecuteBlock(BlockName);
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other == knight.GetComponent<Collider2D>())
+		{
+            StopAllCoroutines();
+		}
+	}
+}
